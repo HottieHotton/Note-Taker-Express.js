6384815caf9934dc927abe0e1b06be5d6fe98d03
Bhotton25 <bhotton25@gmail.com>
2022-01-29 21:04:45 -0700

    Added the first two get routes and they work wonderfully

---
+++ server.js
@@ -0,0 +1,23 @@
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
+const fs = require('fs');
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req , res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+
+app.listen(PORT, () =>{
+console.log('The Website App is listening at port',PORT);
+});
